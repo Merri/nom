5ace6126553c33e963e09f8c93e3c0bfa22b6adc
Vesa Piittinen <vesa.piittinen@gmail.com>
2015-05-23 13:20:20 +0300

    Improve function check performance, fixes for when outside browser env

--- nom.js
+++ nom.js
@@ -1,21 +1,36 @@
-/* Nom version 0.0.11, @license MIT, (c) 2015 Vesa Piittinen */
+/* Nom version 0.0.12, @license MIT, (c) 2015 Vesa Piittinen */
 ;(function(window, isBrowser, hasNode, hasRAF) {
     'use strict';
-    // return at least something when outside browser environment or if no requestAnimationFrame available...
+
+    // give at least something when outside browser environment or if no requestAnimationFrame available...
     function returnNull() { return null; }
-    var nom = { el: returnNull, els: returnNull, mount: returnNull, supported: isBrowser && hasRAF, version: '0.0.11' };
-    // Nom depends on requestAnimationFrame
-    if (!isBrowser || !hasRAF) return nom;
-    // optimize minified JS size
-    var Array = window.Array,
-        document = window.document,
-        flatten = Array.prototype.concat,
-        objectFunction = '[object Function]',
-        objectToString = Object.prototype.toString,
-        // htmlToDOM is HTML generator helper element
-        htmlToDOM = document.createElement('div'),
-        ieActive = {},
-        ltIE10 = 'all' in document && !window.atob;
+
+    var nom = {
+        el: returnNull,
+        els: returnNull,
+        mount: returnNull,
+        supported: isBrowser && hasRAF,
+        version: '0.0.12'
+    };
+
+    // var declares the variables to our scope even if isBrowser === false (but these variables will be undefined)
+    if (isBrowser)
+        // optimize minified JS size
+        var Array = window.Array,
+            document = window.document,
+            flatten = Array.prototype.concat,
+            // Object.prototype.toString.call() is the most compatible way to see if something is a function
+            objFn = '[object Function]',
+            objToStr = Object.prototype.toString,
+            // typeof is fastest way to check if a function but older IEs don't support it for that and Chrome had a bug
+            fn = 'function',
+            canTypeOfFn = typeof returnNull === fn && typeof /./ !== fn,
+            // htmlToDOM is HTML generator helper element
+            htmlToDOM = document.createElement('div'),
+            // helpers for IE
+            ieActive = {},
+            ltIE10 = 'all' in document && !window.atob;
+
     // patch IE9 oninput event bug (does not trigger if deleting value) and use the same code to add IE8- support
     function ieoninput(node) {
         // check that all conditions are met, escape as quickly as possible because this may be called often
@@ -30,9 +45,11 @@
             node.oninput();
         }
     }
+
     // apply the value deletion patch for IE9 and below
-    if ('attachEvent' in document && ltIE10)
+    if (isBrowser && 'attachEvent' in document && ltIE10)
         document.attachEvent('onselectionchange', function() { ieoninput(document.activeElement); });
+
     // applies properties to object or DOM node, adds render method to elements and returns the object
     function render(obj, props, staticProps) {
         var item, originalProps = props, prop, value,
@@ -47,14 +64,14 @@
                 if (!staticProps) render(obj, originalProps);
                 // call render of all the children
                 while (node) {
-                    if (objectToString.call(node.render) === objectFunction)
+                    if (canTypeOfFn ? typeof node.render === fn : objToStr.call(node.render) === objFn)
                         node.render();
 
                     node = node.nextSibling;
                 }
             };
         // if it is a function then assume it returns properties
-        if (objectToString.call(props) === objectFunction)
+        if (canTypeOfFn ? typeof props === fn : objToStr.call(props) === objFn)
             props = props.bind(obj)();
         // if it is a string then assume it is JSON
         else if (typeof props === 'string' && props.charCodeAt(0) === 0x7B) // = '{'
@@ -75,7 +92,7 @@
                     if (window.event.propertyName === 'value') ieoninput(obj);
                 }
             // special case for dealing with children
-            } if (prop === 'children' && obj.childNodes) {
+            } else if (prop === 'children' && obj.childNodes) {
                 node = true;
                 nodeIndex = 0;
                 // shallow flatten to a one dimensional array, eg. [[a], [b, [c]]] -> [a, b, [c]]
@@ -101,7 +118,11 @@
                         // request next node/string
                         node = true;
                     // see if this is a Nom extended node
-                    } else if (objectToString.call(existingNode.render) === objectFunction) {
+                    } else if (
+                        canTypeOfFn
+                        ? typeof existingNode.render === fn
+                        : objToStr.call(existingNode.render) === objFn
+                    ) {
                         // have we ran out of nodes?
                         if (!node) {
                             // abandon ship!
@@ -136,7 +157,7 @@
                     node = nodes[nodeIndex++];
                 }
             // skip functions
-            } else if (objectToString.call(obj[prop]) === objectFunction);
+            } else if (canTypeOfFn ? typeof obj[prop] === fn : objToStr.call(obj[prop]) === objFn);
             // apply subproperties like style if value is an object
             else if (typeof value === 'object') {
                 if (obj[prop] != null)
@@ -152,129 +173,135 @@
         return obj;
     }
 
-    // takes element or creates an element, applies properties to the element ("nomifies it") and returns the element
-    nom.el = function nomElement(element, props, staticProps) {
-        var cssishParts, index = 0;
-        // functions need no processing here
-        if (objectToString.call(props) !== objectFunction) {
-            // sanitate against no props
-            if (props == null)
-                props = {};
-            // default to single assignment of properties
-            if (staticProps == null)
-                staticProps = true;
-            // see if props are really props, make them children if not so
-            switch (typeof props) {
-                case 'object':
-                    // do nothing more if it isn't an array
-                    if (Array.isArray(props))
+    if (isBrowser) {
+        // takes element or creates an element, applies properties to the element and returns the element
+        nom.el = function nomElement(element, props, staticProps) {
+            var cssishParts, index = 0;
+            // functions need no processing here
+            if (canTypeOfFn ? typeof props !== fn : objToStr.call(props) !== objFn) {
+                // sanitate against no props
+                if (props == null)
+                    props = {};
+                // default to single assignment of properties
+                if (staticProps == null)
+                    staticProps = true;
+                // see if props are really props, make them children if not so
+                switch (typeof props) {
+                    case 'object':
+                        // do nothing more if it isn't an array
+                        if (Array.isArray(props))
+                            props = { children: props };
+                        break;
+                    case 'boolean':
+                    case 'number':
+                        props = props.toString();
+                    // intentional to omit break here
+                    default:
                         props = { children: props };
-                    break;
-                case 'boolean':
-                case 'number':
-                    props = props.toString();
-                // intentional to omit break here
-                default:
-                    props = { children: props };
-            }
-        }
-        // see if we need to build an element
-        if (typeof element === 'string') {
-            cssishParts = element.match(/([#.]?[^#.]+)/g);
-            element = document.createElement(cssishParts[0])
-            while (++index < cssishParts.length) {
-                switch(cssishParts[index].charCodeAt(0)) {
-                    case 0x23: // #
-                        element.id = cssishParts[index].slice(1);
-                        break;
-                    case 0x2E: // .
-                        element.className = cssishParts[index].slice(1);
-                        break;
                 }
             }
-        }
-        // assign new properties and add Nom's rendering capabilities to the element
-        return render(element, props, !!staticProps);
-    };
-
-    // takes nodes, HTML strings, object notation elements or arrays of the aforementioned, returns a fragment
-    nom.els = function nomElements(nodes) {
-        var fragment = document.createDocumentFragment(), node, nodeIndex = 0, nodeTag;
-        // create a real array out of everything given
-        nodes = flatten.apply([], arguments);
-        // nodes isn't really containing nodes yet, but we make them be ones
-        while (nodes.length > nodeIndex) {
-            node = nodes[nodeIndex++];
-            // nodes are easy to add in right away
-            if (hasNode ? node instanceof Node : node.nodeType > 0) {
-                fragment.appendChild(node);
-            // HTML strings
-            } else if (typeof node === 'string') {
-                // let a div to the hard work
-                htmlToDOM.innerHTML = node;
-                // capture the children to our fragment
-                while (htmlToDOM.firstChild)
-                    fragment.appendChild(htmlToDOM.firstChild);
-            // recursive call for arrays
-            } else if (Array.isArray(node)) {
-                fragment.appendChild(nomElements.apply(this, node));
-            // object notation elements
-            } else if (typeof node === 'object') {
-                for (nodeTag in node) {
-                    if (node.hasOwnProperty(nodeTag))
-                        fragment.appendChild(nom.el(nodeTag, node[nodeTag]));
+            // see if we need to build an element
+            if (typeof element === 'string') {
+                cssishParts = element.match(/([#.]?[^#.]+)/g);
+                element = document.createElement(cssishParts[0])
+                while (++index < cssishParts.length) {
+                    switch(cssishParts[index].charCodeAt(0)) {
+                        case 0x23: // #
+                            element.id = cssishParts[index].slice(1);
+                            break;
+                        case 0x2E: // .
+                            element.className = cssishParts[index].slice(1);
+                            break;
+                    }
                 }
             }
-        }
-        // rainbows!
-        return fragment;
-    };
+            // assign new properties and add Nom's rendering capabilities to the element
+            return render(element, props, !!staticProps);
+        };
 
-    // takes a fragment or same stuff as els, mounts them for automatic render, returns a fragment
-    nom.mount = function nomMount(fragment) {
-        var mounts = [], node, nodes = [], nodeIndex = 0;
-        // make sure we work with a fragment; support skipping a call to els
-        if (hasNode ? !(fragment instanceof Node && fragment.nodeType === 11) : fragment.nodeType !== 11)
-            fragment = nom.els.apply(this, arguments);
-        // get to know our original children
-        while (fragment.childNodes.length > nodeIndex) {
-            // remember all original childNodes of the fragment so we can restore them to fragment on unmount
-            nodes.push(node = fragment.childNodes[nodeIndex++]);
-            // gather additional reference of Nom rendered element
-            if (objectToString.call(node.render) === objectFunction)
-                mounts.push(node);
-        }
-        // ends rendering and removes all original children from the document and returns the fragment
-        fragment.unmount = function nomUnmount() {
-            // stop render execution by clearing all active mounts
-            mounts.length = 0;
-            // restore all nodes back to the original fragment
-            while (nodes.length)
-                fragment.appendChild(nodes.shift());
-            // return the fragment
+        // takes nodes, HTML strings, object notation elements or arrays of the aforementioned, returns a fragment
+        nom.els = function nomElements(nodes) {
+            var fragment = document.createDocumentFragment(), node, nodeIndex = 0, nodeTag;
+            // create a real array out of everything given
+            nodes = flatten.apply([], arguments);
+            // nodes isn't really containing nodes yet, but we make them be ones
+            while (nodes.length > nodeIndex) {
+                node = nodes[nodeIndex++];
+                // nodes are easy to add in right away
+                if (hasNode ? node instanceof Node : node.nodeType > 0) {
+                    fragment.appendChild(node);
+                // HTML strings
+                } else if (typeof node === 'string') {
+                    // let a div to the hard work
+                    htmlToDOM.innerHTML = node;
+                    // capture the children to our fragment
+                    while (htmlToDOM.firstChild)
+                        fragment.appendChild(htmlToDOM.firstChild);
+                // recursive call for arrays
+                } else if (Array.isArray(node)) {
+                    fragment.appendChild(nomElements.apply(this, node));
+                // object notation elements
+                } else if (typeof node === 'object') {
+                    for (nodeTag in node) {
+                        if (node.hasOwnProperty(nodeTag))
+                            fragment.appendChild(nom.el(nodeTag, node[nodeTag]));
+                    }
+                }
+            }
+            // rainbows!
             return fragment;
-        }
-        // takes care of keeping the nodes up-to-date
-        function render() {
-            var index = mounts.length, mount;
+        };
 
-            while (index) {
-                mount = mounts[--index];
-                // has the node been removed?
-                if (!mount.parentElement)
-                    mounts.splice(index, 1);
-                // are we responsible for the render?
-                else if (objectToString.call(mount.parentElement.render) !== objectFunction)
-                    mount.render();
+        // takes a fragment or same stuff as els, mounts them for automatic render, returns a fragment
+        nom.mount = function nomMount(fragment) {
+            var mounts = [], node, nodes = [], nodeIndex = 0;
+            // make sure we work with a fragment; support skipping a call to els
+            if (hasNode ? !(fragment instanceof Node && fragment.nodeType === 11) : fragment.nodeType !== 11)
+                fragment = nom.els.apply(this, arguments);
+            // get to know our original children
+            while (fragment.childNodes.length > nodeIndex) {
+                // remember all original childNodes of the fragment so we can restore them to fragment on unmount
+                nodes.push(node = fragment.childNodes[nodeIndex++]);
+                // gather additional reference of Nom rendered element
+                if (canTypeOfFn ? typeof node.render === fn : objToStr.call(node.render) === objFn)
+                    mounts.push(node);
             }
-            // keep rendering as long as there is something we can be responsible of
-            if (mounts.length) requestAnimationFrame(render);
-        }
-        // initial render call
-        requestAnimationFrame(render);
-        // magitec!
-        return fragment;
-    };
+            // ends rendering and removes all original children from the document and returns the fragment
+            fragment.unmount = function nomUnmount() {
+                // stop render execution by clearing all active mounts
+                mounts.length = 0;
+                // restore all nodes back to the original fragment
+                while (nodes.length)
+                    fragment.appendChild(nodes.shift());
+                // return the fragment
+                return fragment;
+            }
+            // takes care of keeping the nodes up-to-date
+            function render() {
+                var index = mounts.length, mount;
+
+                while (index) {
+                    mount = mounts[--index];
+                    // has the node been removed?
+                    if (!mount.parentElement)
+                        mounts.splice(index, 1);
+                    // are we responsible for the render?
+                    else if (
+                        canTypeOfFn
+                        ? typeof mount.parentElement.render !== fn
+                        : objToStr.call(mount.parentElement.render) !== objFn
+                    )
+                        mount.render();
+                }
+                // keep rendering as long as there is something we can be responsible of
+                if (mounts.length && hasRAF) requestAnimationFrame(render);
+            }
+            // initial render call
+            if (hasRAF) requestAnimationFrame(render);
+            // magitec!
+            return fragment;
+        };
+    }
 
     // support CommonJS
     if (typeof exports === 'object')
